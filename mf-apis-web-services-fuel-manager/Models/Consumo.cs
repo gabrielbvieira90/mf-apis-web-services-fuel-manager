977ee3ea0ae3abebdf884102bf2590f10ba1f8d4
Gabriel Vieira <126190493+gabrielbvieira90@users.noreply.github.com>
2024-09-13 16:11:51 -0300

    Criei as classes de Veiculo e Consumo

---
+++ mf-apis-web-services-fuel-manager/Models/Consumo.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_apis_web_services_fuel_manager.Models
+{
+    [Table("Consumos")]
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Descricao { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+        [Required]
+        public decimal Valor { get; set; }
+        [Required]
+        public int MyProperty { get; set; }
+        [Required]
+        public TipoCombustivel Tipo { get; set; }
+        [Required]
+        public int VeiculoId { get; set; } // relacionamento entre consumo e veiculos (ForeignKey)
+        public Veiculo Veiculo { get; set; } // navegação virtual, ao carregar a informação do consumo, irá carregar todas as informações do veiculo relacionado a esse consumo
+    }
+
+    public enum TipoCombustivel 
+    {
+        Diesel,
+        Etanol,
+        Gasolina
+    }
+}
