6ff10e9ce990b402d25d45205945a9a8c411c198
Gabriel Vieira <126190493+gabrielbvieira90@users.noreply.github.com>
2024-09-13 17:23:16 -0300

    Configuração do EntityFramework

---
+++ mf-apis-web-services-fuel-manager/Migrations/20240913195424_M01.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mf_apis_web_services_fuel_manager.Migrations
+{
+    /// <inheritdoc />
+    public partial class M01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "Consumos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MyProperty",
+                table: "Consumos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
