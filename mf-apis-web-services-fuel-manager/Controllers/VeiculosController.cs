98c28cadaaad13b22ec0109d7f6990c134d2d995
Gabriel Vieira <126190493+gabrielbvieira90@users.noreply.github.com>
2024-09-18 18:12:50 -0300

    Comando GET API Veiculos

---
+++ mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
@@ -0,0 +1,27 @@
+﻿using mf_apis_web_services_fuel_manager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_apis_web_services_fuel_manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculosController : ControllerBase
+    {
+        private readonly AppDbContext _context; //configuração do banco de dados, variével _context
+
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll() //tarefa assincrona
+        {
+            var model = await _context.Veiculos.ToListAsync();
+            return Ok(model);
+        }
+
+    }
+}
